5d2c060e7ca1c513357800003ff406a3edf5f7e5
Max Millien <maxmillienjr@gmail.com>
2025-04-12 15:55:35 -0400

    add support for tailwindCSS. add initial k8s ingress and deployment

--- apps/web/src/index.jsx
+++ apps/web/src/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Button } from '@storynest/ui';
+import './index.css';
 
 const App = () => (
-  <div>
-    <h1>Welcome to StoryNest</h1>
+  <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50 text-gray-800">
+    <h1 className="text-4xl font-bold mb-6">Welcome to StoryNest</h1>
     <div dangerouslySetInnerHTML={{ __html: Button() }} />
   </div>
 );
