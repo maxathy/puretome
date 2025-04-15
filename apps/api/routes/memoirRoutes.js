57b2fdd3114e60e4e7c71f75ac5fad196bf46dd5
Max Millien <maxmillienjr@gmail.com>
2025-04-15 08:56:30 -0400

    add api route for deleting memoir

--- apps/api/routes/memoirRoutes.js
+++ apps/api/routes/memoirRoutes.js
@@ -6,23 +6,38 @@ const Memoir = require('../models/Memoir');
 
 const { authMiddleware, authorizeRoles } = require('../middleware/auth');
 
-router.post('/',  authMiddleware,
+router.post('/', authMiddleware,
   authorizeRoles('author'), async (req, res) => {
+    try {
+      const { id, ...memoirData } = req.body;
+
+      const updatedMemoir = await Memoir.findOneAndUpdate(
+        { _id: id || new ObjectId() },
+        { ...memoirData },
+        {
+          new: true, upsert: true, runValidators: true,
+        },
+      );
+
+      res.status(!id ? 201 : 200)
+        .json({ message: 'Memoir saved', memoir: updatedMemoir });
+    } catch (err) {
+      console.error('Save error:', err);
+      res.status(500).json({ message: 'Error saving memoir', error: err });
+    }
+  });
+
+router.delete('/', authMiddleware, authorizeRoles('author'), async (req, res) => {
   try {
-    const { id, ...memoirData } = req.body;
-
-    const updatedMemoir = await Memoir.findOneAndUpdate(
-      { _id: id || new ObjectId() },
-      { ...memoirData },
-      { new: true, upsert: true, runValidators: true }
-    );
-
-    res.status(!id?201:200).json({ message: 'Memoir saved', memoir: updatedMemoir });
+    const { id } = req.body;
+    res.status(200)
+      .json({ message: `Memoir ${!id ? 'not found' : 'deleted'}` });
   } catch (err) {
     console.error('Save error:', err);
-    res.status(500).json({ message: 'Error saving memoir', error: err });
+    res.status(500).json({ message: 'Error removing memoir', error: err });
   }
 });
+
 
 router.get('/', authMiddleware, async (req, res) => {
   try {
